846bcd0152b96a16ca9e30bdcdd526b17fad0822
Siddharth Narula <31994579+siddharthnarula1234@users.noreply.github.com>
2021-08-18 15:20:22 -0700

    Create messageusingapplet.java

---
+++ messageusingapplet.java
@@ -0,0 +1,10 @@
+import java.applet.Applet; 
+import java.awt.Graphics; 
+public class applet extends Applet 
+{ 
+	public void paint(Graphics g) 
+	{ 
+		g.drawString("Hello World", 20, 20); 
+	} 	
+}
+
